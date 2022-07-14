aa575a01c56522efab3ec3cc082a3883e01e78e5
s1290085 <s1290085@u-aizu.ac.jp>
2022-07-14 18:32:52 +0900

    Hot: initial revision

---
+++ e4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+  int i=0,cnth=0,cntt=0;
+  printf("tossing a coin...\n");
+  srand(time(NULL));
+  for(i=1;i<4;i++){
+    printf("Round %d:",i);
+    if(rand()%2==0){
+      printf("Heads\n");
+      cnth++;
+    }
+    else{
+    printf("Tails\n");
+    cntt++;
+    }
+  }
+  printf("Heads: %d,Tails: %d\n",cnth,cntt);
+  return 0;
+}
